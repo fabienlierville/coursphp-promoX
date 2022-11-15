116ed865c5caaf4d2b3ccd1b559dfa82db0e0607
fabien <fabien.lierville27@gmail.com>
2022-11-15 17:03:36 +0100

    partie 2

--- www/article_add_script.php
+++ www/article_add_script.php
@@ -1,12 +1,12 @@
 <?php
 
-include('./config.php');
+include('./inc/config.php');
 
-$requete = $bdd->prepare('INSERT INTO articles (Titre, Description, DateAjout, Auteur) 
-    VALUES(:Titre, :Description, :DateAjout, :Auteur)');
+$requete = $bdd->prepare('INSERT INTO articles (Titre, Description, DatePublication, Auteur) 
+    VALUES(:Titre, :Description, :DatePublication, :Auteur)');
 $execute = $requete->execute([
     'Titre' => 'Un autre article'
     , 'Description' => 'Ceci est un nouvel article'
-    , 'DateAjout' => '2019-10-10'
+    , 'DatePublication' => '2019-10-10'
     , 'Auteur' => 'Fabien'
 ]);
