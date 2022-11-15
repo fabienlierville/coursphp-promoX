96ef9ee5bfc62459bb498bd86e635cb82a43b0ff
fabien <fabien.lierville27@gmail.com>
2022-11-15 15:10:45 +0100

    partie 1

--- www/index.php
+++ www/index.php
@@ -8,7 +8,7 @@ require('./inc/header.php');
 $phrase = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tristique eget nulla at eleifend. Integer et sapien eu velit facilisis vestibulum sit amet eu leo. Mauris vitae viverra nunc. Nullam sit amet urna et ante suscipit pharetra. Pellentesque ut finibus quam. Proin vitae laoreet quam, ut pharetra nibh. Sed ornare.";
 
 $extrait = get_words(count: 12,sentence: $phrase )."...";
-var_dump($extrait);
+var_dump($_GET);
 ?>
 
 <?php require('./inc/footer.php'); ?>
