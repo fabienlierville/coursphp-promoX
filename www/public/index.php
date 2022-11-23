a0896788449abc0c0d341b45ce46d337431b6ff1
flierville <flierville@cesi.fr>
2022-11-23 12:00:44 +0100

    Partie 3

--- www/public/index.php
+++ www/public/index.php
@@ -14,5 +14,26 @@ function chargerClasse($classe)
 spl_autoload_register('chargerClasse');
 
 
-$controller = new src\Controller\ArticleController();
-echo $controller->index();
+$controller = (isset($_GET['controller'])) ? $_GET['controller'] : '';
+$action = (isset($_GET['action'])) ? $_GET['action'] : '';
+$param = (isset($_GET['param'])) ? $_GET['param'] : '';
+
+
+if($controller != ''){
+    try {
+        $class = "src\Controller\\".$controller."Controller";
+        if (class_exists($class)) {
+            $controller = new $class();
+            if (method_exists($class, $action)) {
+                echo $controller->$action($param);
+            }else { echo'Acion n\'existe pas pour cette action !';}
+        }else { echo 'Le controlleur n\'existe pas pour cette action !';}
+    }
+    catch(Exception $e) {
+        // Penser à Gérer l’exception
+    }
+}else {
+    //Route par défaut (/)
+    $controller = new \src\Controller\ArticleController();
+    echo $controller->index();
+}
